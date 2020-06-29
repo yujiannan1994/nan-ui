1dd438d3029ba3c6372631553fc2d14f22253068
yujiannan1994 <yujiannan1994@gmail.com>
2020-06-29 15:26:11 +0800

    Add Menu and MenuItem components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Alert from './components/Alert/alert'
+// import Alert from './components/Alert/alert'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   const onClose = () => {
@@ -15,10 +17,21 @@ function App() {
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
         <Button btnType={ButtonType.Link} href="http://ww.baidu.com" target="_blank"> Baidu Link </Button>
         <Button disabled btnType={ButtonType.Link} href="http://ww.baidu.com"> Disabled Link </Button> */}
-        <Alert title="Info Text" description="Info Description Info Description Info Description Info Description" closable onClose={onClose} />
-        <Alert title="Error Text" description="Error Text Error Text" type="danger" closable />
+        {/* <Alert title="Info Text" description="Info Description Info Description Info Description Info Description" closable onClose={onClose} />
+        <Alert title="Error Text" type="danger" closable />
         <Alert title="success Text" description="success Text success Text" type="success" />
-        <Alert title="warning Text" description="warning Text warning Text" type="warning" closable />
+        <Alert title="warning Text" description="warning Text warning Text" type="warning" closable /> */}
+        <Menu defaultIndex={0} mode="vertical" onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0}>
+            cool link
+          </MenuItem>
+          <MenuItem index={1} disabled>
+            cool link 2
+          </MenuItem>
+          <MenuItem index={2}>
+            cool link 3
+          </MenuItem>
+        </Menu>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
